64017f4606efabf340e7e9a0202d025e0f84f19c
auroraB <balseiraurora@comunidad.unam.mx>
2026-06-08 21:13:47 -0600

    Añadida actividad extra: Filtrado de resultados por nombre

--- app/src/main/java/com/example/basedatos/MainActivity.kt
+++ app/src/main/java/com/example/basedatos/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnShowUsers.setOnClickListener {
-            viewModel.cargarUsuarios()
+            viewModel.buscarUsuarios(binding.ettFilter.text.toString())
         }
 
         lifecycleScope.launch {
